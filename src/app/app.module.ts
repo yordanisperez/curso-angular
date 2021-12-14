bd3365fdb2c8660407fb95ced52162d2b860cf4a
Yordanis Perez Brito <yordanis@gmail.com>
2021-12-14 09:17:57 -0500

    Ejercicio Explicativo-1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { Compo1Component } from './compo1/compo1.component';
-import { Compo2Component } from './compo2/compo2.component';
-import { Compo3Component } from './compo3/compo3.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    Compo1Component,
-    Compo2Component,
-    Compo3Component
+
   ],
   imports: [
     BrowserModule
