9f374450954f3f3e63a92c605a3fbbcc39ce0f93
Yordanis Perez Brito <yordanis@gmail.com>
2021-12-15 18:06:28 -0500

    Only aplicativo-3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,32 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { Compo1Component } from './compo1/compo1.component';
-import { Compo2Component } from './compo2/compo2.component';
-import { Compo3Component } from './compo3/compo3.component';
-import { DataBindingComponent } from './data-binding/data-binding.component';
-import { DirNgClassComponent } from './dir-ng-class/dir-ng-class.component';
-import { DirNgIfComponent } from './dir-ng-if/dir-ng-if.component';
-import { DirNgForComponent } from './dir-ng-for/dir-ng-for.component';
-import { DirNgSwitchComponent } from './dir-ng-switch/dir-ng-switch.component';
-import { DirNgModelComponent } from './dir-ng-model/dir-ng-model.component';
-import { DirNgStyleComponent } from './dir-ng-style/dir-ng-style.component';
+
 import { Hijo1Component } from './hijo1/hijo1.component';
 import { Hijo2Component } from './hijo2/hijo2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    Compo1Component,
-    Compo2Component,
-    Compo3Component,
-    DataBindingComponent,
-    DirNgClassComponent,
-    DirNgIfComponent,
-    DirNgForComponent,
-    DirNgSwitchComponent,
-    DirNgModelComponent,
-    DirNgStyleComponent,
     Hijo1Component,
     Hijo2Component
   ],
