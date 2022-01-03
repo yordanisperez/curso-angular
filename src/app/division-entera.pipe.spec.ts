32f27dc026f22eb6c23867b1fdb02ac3bbf76b1e
Yordanis Perez Brito <yordanis@gmail.com>
2022-01-03 10:02:18 -0500

    Aplicativo Reactivo clear

--- src/app/division-entera.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { DivisionEnteraPipe } from './division-entera.pipe';
-
-describe('DivisionEnteraPipe', () => {
-  it('create an instance', () => {
-    const pipe = new DivisionEnteraPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
