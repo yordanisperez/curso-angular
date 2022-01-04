77d9e11b7ca0633cf60f0740d7ea4bfa16b8d5f8
Yordanis Perez Brito <yordanis@gmail.com>
2022-01-04 15:26:45 -0500

    Aplicativo-5 Clear

--- src/app/ci-to-date.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { CiToDatePipe } from './ci-to-date.pipe';
-
-describe('CiToDatePipe', () => {
-  it('create an instance', () => {
-    const pipe = new CiToDatePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
